af213c44c4d5722751e0004ab4132a2ffa32703c
AquaJerry <tongzhan_liuzeyang@qq.com>
2022-02-28 09:57:01 +0800

    redesign: cache dict of studio instead of list

--- fetchmusic.py
+++ fetchmusic.py
@@ -1,19 +1,19 @@
 #!/bin/python3
+from time import sleep, time
 import json
 import os
-import time
 
+
+# Obey API rate limit
+last_fetch_time = time()
 
 # Cache info about anime companies
-studios_filename = 'studios.json'
+studio_dict_fname = 'studio_dict.json'
 
 # From which get anime songs
-api_moe = 'https://staging.animethemes.moe/api/'
+api_head = 'https://staging.animethemes.moe/api/'
 
-# Use this to get list of anime companies
-studio_moe = api_moe + 'studio?fields[studio]=id,name,slug'
-
-
+# Use this to get animes on air in some year
 year_show_param = '?' + '&'.join([
     'fields[anime]=id',
     'include=' + ','.join([
@@ -23,46 +23,48 @@ year_show_param = '?' + '&'.join([
 ])
 
 
-def fetch_fullmoe(fullmoe):
+def fetch_by_api(api):
     '''Help you write less to fetch things from animethemes.moe.'''
-    return json.loads(os.popen(f"curl '{fullmoe}'").read())
+    global last_fetch_time
+    if 1 > time() - last_fetch_time:
+        sleep(1)
+    pipe = os.popen(f"curl '{api}'")
+    last_fetch_time = time()
+    return json.loads(pipe.read())
 
 
-def fetch_moe(moe):
+def fetch_by_api_body(api_body):
     '''Help you write less to fetch things from animethemes.moe.'''
-    return fetch_fullmoe(api_moe + moe)
+    return fetch_by_api(api_head + api_body)
 
 
-def get_studios_per_page(moe, old_studios):
-    '''Fetch info about anime companies once a page.
+def fetch_studios(api, dict):
+    '''Fetch info about anime companies page by page.
     This will fetch all companies online.
     '''
-    studio_index = fetch_fullmoe(moe)
-    print(f"Studio page {studio_index['meta']['current_page']} fetch")
-    new_studios = studio_index['studios']
-    studios = [*old_studios, *new_studios]
-    if next := studio_index['links']['next']:
-        time.sleep(1)  # Obey API rate limit
-        studios = get_studios_per_page(next, studios)
-    return studios
+    page = fetch_by_api(api)
+    dict = {**dict, **{s['id']: {'slug': s['slug']} for s in page['studios']}}
+    if next := page['links']['next']:
+        dict = fetch_studios(next, dict)
+    return dict
 
 
-def get_all_studios():
-    '''Tell me companies who make animes
+def get_studio_dict():
+    '''Get a dict of companies who make animes
     '''
     try:
-        with open(studios_filename) as f:
-            studios = json.load(f)
+        with open(studio_dict_fname) as f:
+            dict = json.load(f)
     except:
-        studios = get_studios_per_page(studio_moe, [])
-        with open(studios_filename, 'w') as f:
+        with open(studio_dict_fname, 'w') as f:
             # Save disk usage by sep
-            json.dump(studios, f, separators=(',', ':'))
-    return studios
+            api = api_head + 'studio?fields[studio]=id,slug'
+            json.dump(dict := fetch_studios(api, {}), f)
+    return dict
 
 
 if '__main__' == __name__:
     # Test script here
-    get_all_studios()
+    get_studio_dict()
     #moe = fetch_moe(f'animeyear/{year}{year_show_param}')
     # print(moe)
