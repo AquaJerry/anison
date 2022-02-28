fcf111c68cc1f6bbfe3e8f5aa7e93b96b1189d9d
AquaJerry <tongzhan_liuzeyang@qq.com>
2022-02-28 17:59:21 +0800

    feat: Short name for anime companies

--- fetchmusic.py
+++ fetchmusic.py
@@ -1,70 +1,84 @@
 #!/bin/python3
-from time import sleep, time
 import json
 import os
+import re
+import time
 
 
-# Obey API rate limit
-last_fetch_time = time()
-
-# Cache info about anime companies
-studio_dict_fname = 'studio_dict.json'
-
-# From which get anime songs
-api_head = 'https://staging.animethemes.moe/api/'
-
-# Use this to get animes on air in some year
-year_show_param = '?' + '&'.join([
-    'fields[anime]=id',
-    'include=' + ','.join([
-        'animethemes.animethemeentries.videos',
-        'studios',
-    ]),
-])
-
-
-def fetch_by_api(api):
-    '''Help you write less to fetch things from animethemes.moe.'''
-    global last_fetch_time
-    if 1 > time() - last_fetch_time:
-        sleep(1)
-    pipe = os.popen(f"curl '{api}'")
-    last_fetch_time = time()
-    return json.loads(pipe.read())
-
-
-def fetch_by_api_body(api_body):
-    '''Help you write less to fetch things from animethemes.moe.'''
-    return fetch_by_api(api_head + api_body)
-
-
-def fetch_studios(api, dict):
-    '''Fetch info about anime companies page by page.
-    This will fetch all companies online.
+class Angel:
+    '''Fetcher
     '''
-    page = fetch_by_api(api)
-    dict = {**dict, **{s['id']: {'slug': s['slug']} for s in page['studios']}}
-    if next := page['links']['next']:
-        dict = fetch_studios(next, dict)
-    return dict
+    # From which get anime songs
+    api_head = 'https://staging.animethemes.moe/api/'
+
+    # Obey API rate limit
+    last_fetch_time = time.time()
+
+    def fetch_by_api(api):
+        '''Help you write less to fetch things from animethemes.moe.'''
+        if 1 > time.time() - Angel.last_fetch_time:
+            time.sleep(1)
+        pipe = os.popen(f"curl '{api}'")
+        Angel.last_fetch_time = time.time()
+        return json.loads(pipe.read())
+
+    def fetch_by_api_body(api_body):
+        '''Help you write less to fetch things from animethemes.moe.'''
+        return Angel.fetch_by_api(Angel.api_head + api_body)
 
 
-def get_studio_dict():
-    '''Get a dict of companies who make animes
+class StudioBook(dict):
+    '''Look up its instance to get info about a company
     '''
-    try:
-        with open(studio_dict_fname) as f:
-            dict = json.load(f)
-    except:
-        with open(studio_dict_fname, 'w') as f:
-            # Save disk usage by sep
-            api = api_head + 'studio?fields[studio]=id,slug'
-            json.dump(dict := fetch_studios(api, {}), f)
-    return dict
+    def __init__(self):
+        '''Get a dict of companies who make animes
+        '''
+        fname = 'studio_book.json'
+        try:
+            with open(fname) as f:
+                for k, v in json.load(f).items():
+                    self[k] = v
+        except:
+            with open(fname, 'w') as f:
+                # Save disk usage by sep
+                api = Angel.api_head + 'studio?fields[studio]=id,slug'
+                json.dump(self.fetch(api).abbreviate(), f)
+
+    def abbreviate(self):
+        '''Short names are better on wearables
+        '''
+        pres = 'animation', 'production', 'project', 'studio', 'team', 'tokyo'
+        for studio in self.values():
+            full = studio['slug'].replace('_', '')
+            abbr = full[:3]
+            if full.startswith(pres):
+                pattern = f'({"|".join(pres)})' r'(?P<abbr>\w{,3})'
+                abbr = re.compile(pattern).match(full).group('abbr')
+            studio['abbr'] = abbr
+        return self
+
+    def fetch(self, api):
+        '''Fetch info about anime companies page by page.
+        This will fetch all companies online.
+        '''
+        page = Angel.fetch_by_api(api)
+        for studio in page['studios']:
+            self[studio['id']] = {'slug': studio['slug']}
+        if next := page['links']['next']:
+            self.fetch(next)
+        return self
 
 
 if '__main__' == __name__:
     # Test script here
-    get_studio_dict()
-    #moe = fetch_moe(f'animeyear/{year}{year_show_param}')
-    # print(moe)
+    StudioBook()
+    #moe = Angel.fetch_by_api_body(f'animeyear/{year}{year_show_param}')
+    #print(moe)
+    # Use this to get animes on air in some year
+    #year_show_param = '?' + '&'.join([
+    #    'fields[anime]=id',
+    #    'include=' + ','.join([
+    #        'animethemes.animethemeentries.videos',
+    #        'studios',
+    #    ]),
+    #])
