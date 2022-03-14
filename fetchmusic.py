2ddec7c669446f707b2762f720533d1c76563beb
AquaJerry <tongzhan_liuzeyang@qq.com>
2022-03-14 09:00:28 +0800

    fix: filename conflicts

--- fetchmusic.py
+++ fetchmusic.py
@@ -1,4 +1,5 @@
 #!/bin/python3
+import datetime
 import hashlib
 import json
 import os
@@ -28,11 +29,24 @@ class Angel:
 
     def pull(self, link, name):
         '''Download link as name.mp3'''
+        suffix = '.mp3'
+        tmp_out_path = f'{name}z{suffix}'
         self.ready()
-        os.system(f'curl {link} -o a.webm;'
+        os.system(f'curl -k {link} -o a.webm;'
                   f'ffmpeg -i a.webm -af loudnorm -b:a 64k'
-                  f' -map_chapters -1 -map_metadata -1 {name}.mp3'
-                  f';rm a.webm')
+                  f' -map_chapters -1 -map_metadata -1 -y {tmp_out_path};'
+                  f'rm a.webm;')
+        if 1 < (slen := len(sames := [*pathlib.Path().glob(f'{name}*')])):
+            # solve name conflicts
+            mds = (hashlib.md5(s.read_bytes()).hexdigest() for s in sames)
+            for i in range(2):  # conflict rate: 1/256
+                if len({m[:1+i] for m in mds}) == slen:
+                    for i, s in enumerate(sames):
+                        s.rename(f'{name}{mds[i]}{suffix}')
+            else:
+                pathlib.Path(tmp_out_path).unlink()  # remove
+        else:
+            pathlib.Path(tmp_out_path).rename(f'{name}{suffix}')  # save
 
     def ready(self):
         '''Most websites keep a rate limit of 60 per minute'''
@@ -89,32 +103,71 @@ class StudioBook(dict):
         return self.rawfetch(self.angel.api_head + api_body)
 
 
+class AnimeSeason:
+    Season = 'Winter', 'Spring', 'Summer', 'Fall'
+
+    def __init__(self, year = 1963, quarter = 0):
+        '''The first anime is on air in 1963'''
+        self.quarter = quarter
+        self.season = AnimeSeason.Season[quarter]
+        self.year = year
+    
+    def __iter__(self):
+        return self
+
+    def __repr__(self):
+        return f'{str(self.year)[-2:]}{1 + self.quarter}'
+    
+    def __next__(self):
+        self.quarter += 1
+        if len(AnimeSeason.Season) == self.quarter:
+            self.quarter = 0
+            self.year += 1
+            if datetime.date.today().year < self.year:
+                raise StopIteration
+        return self
+
+
 class AnimeAngel:
     def __init__(self):
         self.angel = Angel('https://staging.animethemes.moe/api/')
         self.book = StudioBook(self.angel)
 
-    def clone_songs(self):
-        # The first anime is on air in 1963
-        for year in range(1963, 2022):
-            for quarter in range(4):
-                self.pull_songs_in(quarter=quarter, year=year)
+    def clone_songs(self, since = AnimeSeason()):
+        if since is not AnimeSeason:
+            since = AnimeSeason(since)
+        for season in since:
+            self.pull_songs_in(season)
 
-    def pull_songs_in(self, quarter, year):
+    def pull(self, endpoint, config={}):
+        params = []
+        for k, v in config.items():
+            if type(v) is str:
+                params += [f'{k}={v}']
+            else:
+                params += [f'{k}[{s}]={w}' for s, w in v.items()]
+        self.rawpull(f'{self.angel.api_head}{endpoint}'
+                     f"{'?' if params else ''}{'&'.join(params)}")
+
+    def pull_songs_in(self, when):
         '''pull anime songs from animethemes.moe'''
         # Use this to get animes on air in some year
-        param = '&'.join([
-            'fields[anime]=id',  # useless
-            'fields[animetheme]=slug',  # sluglikeOP1
-            'fields[animethemeentry]=id',  # useless
-            'fields[studio]=id',  # For lookup abbr of companies
-            'fields[video]=filename,size',
-            f"filter[season]={('Winter', 'Spring', 'Summer', 'Fall')[quarter]}",
-            f'filter[year]={year}',
-            'include=animethemes.animethemeentries.videos,studios',
-        ])
-        self.when = f'{str(year)[-2:]}{1 + quarter}'
-        self.rawpull(self.angel.api_head + f'anime?{param}')
+        self.when = when
+        self.pull('anime', {
+            'fields': {
+                'anime': 'id',  # useless
+                'animetheme': 'slug',  # sluglikeOP1
+                'animethemeentry': 'id',  # useless
+                'studio': 'id',  # for looking up abbrs of company
+                'video': 'filename,size',
+            },
+            'filter': {
+                'has': 'studios',
+                'season': when.season,
+                'year': when.year,
+            },
+            'include': 'animethemes.animethemeentries.videos,studios',
+        })
 
     def rawpull(self, api):
         moe = self.angel.rawfetch(api)
@@ -125,30 +178,13 @@ class AnimeAngel:
                 slug = animetheme['slug']
                 slug = slug[0] + slug[2:]
                 videos = animetheme['animethemeentries'][0]['videos']
-                key = lambda video: video['size']
+                def key(video): return video['size']
                 filename = sorted(videos, key=key)[0]['filename']
                 link = f'https://animethemes.moe/video/{filename}.webm'
                 name = f'{self.when}{studiostr}{slug}'
-                to_rename = 0
-                if [*pathlib.Path().glob(f'{name}*')]:
-                    to_rename = 1
-                    if pathlib.Path(name).exists():
-                        self.rename(name)
                 self.angel.pull(link, name)
-                if to_rename:
-                    self.rename(name)
-            pass
         if next := moe['links']['next']:
             self.rawpull(next)
-
-    def rename(self, name):
-        path = pathlib.Path(name)
-        md = hashlib.md5(path.read_bytes()).hexdigest()
-        for i in range(32):
-            newname = f'{name}{md[:1+i]}'
-            if not [*pathlib.Path().glob(f'{newname}*')]:
-                path.rename(newname)
-                break
 
 
 if '__main__' == __name__:
