0a3467cc0194e6c6231cfe5c696a4b844516f526
AquaJerry <tongzhan_liuzeyang@qq.com>
2022-03-17 20:04:44 +0800

    refactor: support skipping downloaded

--- fetchmusic.py
+++ fetchmusic.py
@@ -1,83 +1,65 @@
 #!/bin/python3
-import datetime
+import argparse
 import hashlib
 import json
 import os
 import pathlib
 import re
-import sys
 import time
 
 
 class Angel:
     '''Fetcher'''
+    api_head = 'https://staging.animethemes.moe/api/'
+    last_fetch_time = time.time()  # Obey API rate limit
 
-    def __init__(self, api_head):
-        self.api_head = api_head
-        self.last_fetch_time = time.time()  # Obey API rate limit
-
-    def rawfetch(self, api):
+    def fetch(api):
         '''Help you write less to fetch things from animethemes.moe'''
-        self.ready()
-        try:
-            return json.loads(os.popen(f"curl -g '{api}'").read())
-        except:
-            return self.rawfetch(api)
+        Angel.ready()
+        return json.loads(os.popen(f"curl -g '{api}'").read())
 
-    def fetch(self, api_body):
-        '''Help you write less to fetch things from animethemes.moe'''
-        return self.rawfetch(self.api_head + api_body)
-
-    def pull(self, link, name):
+    def pull(link, name, not_overwrite):
         '''Download link as name.mp3'''
-        suffix = '.mp3'
-        tmp_out_path = f'{name}z{suffix}'
-        self.ready()
-        incomplete = os.system(
-            f'curl {link} | '
-            f'ffmpeg -i - -af loudnorm -b:a 64k'
-            f' -map_chapters -1 -map_metadata -1 {tmp_out_path}'
-        )
-        if incomplete:
-            os.system(f'rm {tmp_out_path}')
+        if (sames := [*pathlib.Path().glob(f'{name}*')]) and not_overwrite:
             return
-        if 1 < (slen := len(sames := [*pathlib.Path().glob(f'{name}*')])):
+        Angel.ready()
+        if os.system(f'curl {link}|ffmpeg -i - -af loudnorm -b:a 64k'
+                     f' -map_chapters -1 -map_metadata -1 -f mp3 /tmp/{name}'):
+            return  # download incomplete
+        if sames:
             # solve name conflicts
+            sames += [pathlib.Path(f'/tmp/{name}')]
             mds = [hashlib.md5(s.read_bytes()).hexdigest() for s in sames]
             for i in range(2):  # conflict rate: 1/256
-                if len({m[:1+i] for m in mds}) == slen:
+                if len(sames) == len({m[:1+i] for m in mds}):
                     for j, s in enumerate(sames):
-                        s.rename(f'{name}{mds[j][:1+i]}{suffix}')
+                        s.rename(f'{name}{mds[j][:1+i]}.mp3')
                     break
-            else:
-                pathlib.Path(tmp_out_path).unlink()  # remove
         else:
-            pathlib.Path(tmp_out_path).rename(f'{name}{suffix}')  # save
+            os.system(f'mv /tmp/{name} {name}.mp3')  # save
 
-    def ready(self):
+    def ready():
         '''Most websites keep a rate limit of 60 per minute'''
-        if 1 > time.time() - self.last_fetch_time:
+        if 1 > time.time() - Angel.last_fetch_time:
             time.sleep(1)
-        self.last_fetch_time = time.time()
+        Angel.last_fetch_time = time.time()
 
 
 class StudioBook(dict):
-    '''Look up its instance to get info about a company
-    '''
+    '''Look up its instance to get info about a company'''
 
-    def __init__(self, angel):
+    def __init__(self):
         '''Get a dict of companies who make animes
         '''
-        self.angel = angel
         book = pathlib.Path('studio_book.json')
         if book.exists():
             with book.open() as f:
                 for k, v in json.load(f).items():
                     self[k] = v
         else:
-            newbook = self.fetch('studio?fields[studio]=id,slug').abbr()
+            api = Angel.api_head + 'studio?fields[studio]=id,slug'
             with book.open('w') as f:
-                json.dump(newbook, f)
+                json.dump(self.pull(api).abbr(), f)
 
     def abbr(self):
         '''Short names are better on wearables
@@ -94,19 +76,16 @@ class StudioBook(dict):
             studio['abbr'] = abbr.upper()   # upper is more readable
         return self
 
-    def rawfetch(self, api):
+    def pull(self, api):
         '''Fetch info about anime companies page by page.
         This will fetch all companies online.
         '''
-        page = self.angel.rawfetch(api)
+        page = Angel.fetch(api)
         for studio in page['studios']:
             self[studio['id']] = {'slug': studio['slug']}
         if next := page['links']['next']:
-            self.rawfetch(next)
+            self.pull(next)
         return self
-
-    def fetch(self, api_body):
-        return self.rawfetch(self.angel.api_head + api_body)
 
 
 class AnimeSeason:
@@ -116,9 +95,9 @@ class AnimeSeason:
         if 'season' == name:
             return self.Season[self.quarter]
 
-    def __init__(self, season = 0):
+    def __init__(self, season=''):
         '''The first anime is on air in 1st quarter in 1963'''
-        if not re.compile(r'\d\d[1-4]').match(season or ''):
+        if not re.compile(r'\d\d[1-4]').match(season):
             season = '631'
         self.quarter = int(season[2]) - 1
         self.year = (y := int(season[:2])) + (1900, 2000)[63 > y]
@@ -130,23 +109,35 @@ class AnimeSeason:
             if len(self.Season) == self.quarter:
                 self.quarter = 0
                 self.year += 1
-                if datetime.date.today().year < self.year:
-                    break
 
     def __repr__(self):
         return f'{str(self.year)[-2:]}{1 + self.quarter}'
 
 
 class AnimeAngel:
-    def __init__(self):
-        self.angel = Angel('https://staging.animethemes.moe/api/')
-        self.book = StudioBook(self.angel)
+    book = StudioBook()
 
-    def clone_songs(self, since=0):
-        if type(since) is not AnimeSeason:
-            since = AnimeSeason(since)
-        for season in since:
-            self.pull_songs_in(season)
+    def clone_songs(self, since='', not_overwrite=0):
+        self.not_overwrite = not_overwrite
+        for when in AnimeSeason(since):
+            '''pull anime songs from animethemes.moe'''
+            # Use this to get animes on air in some year
+            self.when = when
+            self.pull('anime', {
+                'fields': {
+                    'anime': 'id',  # useless
+                    'animetheme': 'slug',  # sluglikeOP1
+                    'animethemeentry': 'id',  # useless
+                    'studio': 'id',  # for looking up abbrs of company
+                    'video': 'filename,size',
+                },
+                'include': (i := 'animethemes.animethemeentries.videos,studios'),
+                'filter': {
+                    'has-and': i,
+                    'season': when.season,
+                    'year': when.year,
+                },
+            })
 
     def pull(self, endpoint, config={}):
         params = []
@@ -155,46 +146,29 @@ class AnimeAngel:
                 params += [f'{k}={v}']
             else:
                 params += [f'{k}[{s}]={w}' for s, w in v.items()]
-        self.rawpull(f'{self.angel.api_head}{endpoint}'
+        self.rawpull(f'{Angel.api_head}{endpoint}'
                      f"{'?' if params else ''}{'&'.join(params)}")
 
-    def pull_songs_in(self, when):
-        '''pull anime songs from animethemes.moe'''
-        # Use this to get animes on air in some year
-        self.when = when
-        self.pull('anime', {
-            'fields': {
-                'anime': 'id',  # useless
-                'animetheme': 'slug',  # sluglikeOP1
-                'animethemeentry': 'id',  # useless
-                'studio': 'id',  # for looking up abbrs of company
-                'video': 'filename,size',
-            },
-            'filter': {
-                'has': 'studios',
-                'season': when.season,
-                'year': when.year,
-            },
-            'include': 'animethemes.animethemeentries.videos,studios',
-        })
-
     def rawpull(self, api):
-        moe = self.angel.rawfetch(api)
+        moe = Angel.fetch(api)
         for anime in moe['anime']:
             studiostr = ''.join(self.book[str(s['id'])]['abbr']
                                 for s in anime['studios'])
             for animetheme in anime['animethemes']:
-                slug = animetheme['slug']
-                slug = slug[0] + slug[2:]
+                slug = (s := animetheme['slug'])[0] + s[2:]
                 videos = animetheme['animethemeentries'][0]['videos']
                 def key(video): return video['size']
                 filename = sorted(videos, key=key)[0]['filename']
                 link = f'https://animethemes.moe/video/{filename}.webm'
                 name = f'{self.when}{studiostr}{slug}'
-                self.angel.pull(link, name)
+                Angel.pull(link, name, self.not_overwrite)
         if next := moe['links']['next']:
             self.rawpull(next)
 
 
 if '__main__' == __name__:
-    AnimeAngel().clone_songs(sys.argv[1] if 1 - len(sys.argv) else 0)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('since', default='', nargs='?')
+    parser.add_argument('-n', action='store_true')
+    arg = parser.parse_args()
+    AnimeAngel().clone_songs(arg.since, arg.n)
