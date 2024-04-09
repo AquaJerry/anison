bafc1b1c9379ef47001b821b44588a583909b7fc
aquajerry <tongzhan_liuzeyang@qq.com>
2024-04-10 00:56:00 +0800

    doc: skip existing songs always

--- anison.py
+++ anison.py
@@ -1,11 +1,12 @@
 #!/bin/python3
 import argparse, json, os, re, time
 def abbr(slug):
-    '''Short names are better on wearables'''
+    '''e.g. mushi_production -> MUS, studio_ghibli -> GHI, production_ig -> PIG'''
     abbr = re.match('(production|studio|)(.{,3})', full := re.sub('\W|_', '', slug))[2]  # abbr is alnum
     if 3 > (l := len(abbr)): abbr = f'{full[:3-l]}{abbr}'  # each abbr lens 3
     return abbr.upper()   # upper is more readable
 class Season:
+    '''e.g. for season in Season()'''
     def __getattr__(self, name):
         match name:
             case 'season': return ('Winter', 'Spring', 'Summer', 'Fall')[self.quarter]
@@ -27,7 +28,7 @@ class Song:
     def __init__(self, since=''):
         self.last_curl_time = time.time()  # Obey API rate limit
         for when in Season(since):
-            '''pull anime songs from animethemes.moe'''
+            '''pull anison from animethemes.moe'''
             # Use this to get animes on air in some year
             self.when = when
             include = 'animethemes.animethemeentries.videos.audio,studios'
@@ -36,7 +37,7 @@ class Song:
                 '&fields[animetheme]=sequence,type'  # like'1,OP'
                 '&fields[animethemeentry]=id'  # useless
                 '&fields[audio]=filename,size'
-                '&fields[studio]=slug'
+                '&fields[studio]=slug'  # like'studio_ghibli'
                 '&fields[video]=id'  # useless
                 f"&filter[has-and]={include}"
                 f'&filter[season]={when.season}'
@@ -55,21 +56,19 @@ class Song:
             s = ''.join(abbr(s['slug']) for s in a['studios'])
             for t in a['animethemes']:
                 name = f"{self.when}{s}{t['type'][0]}{t['sequence']or''}"
-                if sames := os.popen(f'ls {name}[.a-z]*').read().split():
-                    continue
-                a = [v['audio'] for e in t['animethemeentries'] for v in e['videos']]
-                f = sorted(a, key=lambda a: a['size'])[0]['filename']
-                # Download link as name.mp3
-                # If skip, list local songs to try reduce download times
-                # > name = '631MUSO'
-                # > ls $name[.a-z]*
+                # list local songs to try reduce download times
+                # > ls 631MUSO[.a-z]*
                 # 631MUSO.mp3
                 # 631MUSOa.mp3
                 #        ^ The dot(.) and lowercase means no need to download
+                if sames := os.popen(f'ls {name}[.a-z]*').read().split():
+                    continue  # skip existing song
+                a = [v['audio'] for e in t['animethemeentries'] for v in e['videos']]
+                f = sorted(a, key=lambda a: a['size'])[0]['filename']
+                # Download name.mp3
                 if self.curl(lambda: os.system(f'curl https://a.animethemes.moe/{f}.ogg|ffmpeg -i - -af loudnorm -b:a 64k -map_chapters -1 -map_metadata -1 -f mp3 /tmp/{name}')):
                     break  # download incomplete
-                if sames:
-                    # solve name conflicts
+                if sames:  # then solve name conflicts
                     sames += f'/tmp/{name}',
                     mds = [''.join(chr(55+ord(c)) if c.isdigit() else c for c in
                                 os.popen(f'md5sum {s}').read()) for s in sames]
