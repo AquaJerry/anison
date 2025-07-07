c573ce466022566a45f242e8b385f56fa35e81dc
aquajerry <zy0yc@qq.com>
2025-07-07 16:09:56 +0800

    fix: missing audio by switching endpoint from 'anime index' to 'year index'

--- anison.py
+++ anison.py
@@ -17,36 +17,34 @@ same = lambda name: os.popen(f'ls {name}[.a-z]*').read().split()
 p = argparse.ArgumentParser()
 p.add_argument('since', default='631', nargs='?')  # what season to begin
 a = p.parse_args()
-when = int(a.since[2]) - 1 + 4 * (int(a.since[:2]) + (-63, 37)[63 > int(a.since[:2])])  # what season now
+season_arg, year = int(a.since[2]) - 1, (y := int(a.since[:2])) + (1899, 1999)[63 > y]  # season 0: winter, 1: spring, 2: summer, 3: fall
 newest_date = os.popen('date').read()  # see: if themes updated remote
-while 1:
-    girl, moe = {}, {'links': {'next': 'https://api.animethemes.moe/anime'
+while moe := json.load(os.popen("sleep 1;curl -g 'https://api.animethemes.moe/"  # sleep to obey rate limit
+        f'animeyear/{str(year := 1 + year)}'
         '?fields[anime]=id'  # useless
         '&fields[animetheme]=sequence,type'  # like'1,OP'
         '&fields[animethemeentry]=id'  # useless
         '&fields[audio]=filename,size'
         '&fields[studio]=slug'  # like'studio_ghibli'
         '&fields[video]=id'  # useless
-        '&filter[has-and]=animethemes.animethemeentries.videos.audio,studios'
-        f"&filter[season]={('winter', 'spring', 'summer', 'fall')[when % 4]}"
-        f'&filter[year]={1963 + when // 4}'
         '&include=animethemes.animethemeentries.videos.audio,studios'
-        }}  # Use this to get anison on air in some season
-    while pg := moe['links']['next']:
-        for a in (moe := json.load(os.popen(f"sleep 1;curl -g '{pg}'")))['anime']:  # sleep to obey rate limit
+        "'")):  # Use this to get anison on air in some year
+    for season in range(season_arg, 4):
+        girl = {}
+        for a in moe.get(('winter', 'spring', 'summer', 'fall')[season], ()):
             s = ''.join(abbr(s['slug']) for s in a['studios'])
             for t in a['animethemes']:
-                if sames := same(name := f"{str(1963+when//4)[-2:]}{1+when%4}{s}{t['type'][0]}{t['sequence']or''}"):
+                if sames := same(name := f"{str(year)[-2:]}{1+season}{s}{t['type'][0]}{t['sequence']or''}"):
                     girl.setdefault(name, []).append(t)  # do later
                 else: os.system(f'{mp3(name, t)}&&mv /tmp/{name} {name}.mp3')  # save if complete
-    for name, themes in girl.items():
-        if len(themes) > int(os.popen(f"find ! -newerat '{newest_date}' -name {name}[.a-z]*|wc -l").read()):  # if themes updated remote
-            for theme in themes:  # tell from mds, like'631MUSEa.mp3,631MUSEb.mp3'
-                os.system(mp3(name, theme))  # cache /tmp/$name
-                sames = *same(name), f'/tmp/{name}'  # must re-eval sames each time
-                mds = [''.join(chr(55+ord(c))if c.isdigit()else c for c in os.popen(f'md5sum {s}').read())for s in sames]
-                for i in range(2):  # conflict rate: 1/256
-                    if len(sames) == len({m[:1+i] for m in mds}):  # mds='a,b'or'aa,ab'
-                        for j, s in enumerate(sames): os.system(f'mv {s} {name}{mds[j][:1+i]}.mp3')
-                        break
-    when += 1  # next season
+        for name, themes in girl.items():
+            if len(themes) > int(os.popen(f"find ! -newerat '{newest_date}' -name {name}[.a-z]*|wc -l").read()):  # if themes updated remote
+                for theme in themes:  # tell from mds, like'631MUSEa.mp3,631MUSEb.mp3'
+                    os.system(mp3(name, theme))  # cache /tmp/$name
+                    sames = *same(name), f'/tmp/{name}'  # must re-eval sames each time
+                    mds = [''.join(chr(55+ord(c))if c.isdigit()else c for c in os.popen(f'md5sum {s}').read())for s in sames]
+                    for i in range(2):  # conflict rate: 1/256
+                        if len(sames) == len({m[:1+i] for m in mds}):  # mds='a,b'or'aa,ab'
+                            for j, s in enumerate(sames): os.system(f'mv {s} {name}{mds[j][:1+i]}.mp3')
+                            break
+    season_arg = 0  # next year
