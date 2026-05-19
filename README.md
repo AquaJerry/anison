0cdec1889f0364a218b564bf8ef6d9d8be78e770
zy0yc <zy0yc@qq.com>
2026-05-19 18:58:06 +0800

    refactor: unattended update

--- README.md
+++ README.md
@@ -1,8 +1,45 @@
-# Anime songs on watch
+# Anison
 
-This repo is to download some anime musics onto user's wearables.
+Keep your anison (animation/donghua/anime/aeni music) collection up to date.
 
-Work in process.
+Works (maybe?) on systems like:
+
+- __iOS__ — via UTM (unofficial) or iSH (unofficial)
+- __Apple Container__ — Linux container environment on macOS
+- __Android Terminal__ — Built‑in Linux terminal (Available since newer Android phones/tablets/PCs)
+- __WSL__ — Windows Subsystem for Linux
+- __Huawei Converged Development Engine__ — 华为融合开发引擎
+- __Honor Linux Lab__ — 荣耀 Linux 实验室
+
+
+## Get Started
+
+Right now, the easiest way to start is to place the file `animethemes-audios` into one of these folders: `/etc/cron.hourly` or `/etc/cron.daily`.
+
+⚠️ __Note:__ This manual step is temporary. A fully automatic step is planned for the future.
+
+Once placed, your system will update the anison by itself.
+
+
+## Change the Anison Folder
+
+Anison is saved to your __Home folder__ by default, in a folder called `animethemes-audios`.
+
+If you want to save anison somewhere else (like an external drive), you can connect the new folder to this location through your __File Manager__:
+
+1. Go to the folder where you want to keep your music.
+2. __Right‑click__ that folder and choose __“Create Link”__ or __“Make Link”__.
+3. Move that link into your Home folder and rename it to: `animethemes-audios`.
+
+This acts like a __shortcut__, so the updater finds your anison without knowing exactly where it is saved.
+
+
+## Contributing
+
+Please contribute to:
+
+- https://github.com/AnimeThemes
+
 
 ## Repo Addresses
 
--- anison.py
+++ anison.py
@@ -1,50 +1,32 @@
 #!/bin/python3
-import argparse, json, os, re
-def abbr(s):
-    '''e.g. mushi_production -> MUS, studio_ghibli -> GHI, production_ig -> PIG'''
-    abbr = re.match('(production|studio|)(.{,3})', full := re.sub('\W|_', '', s))[2]  # abbr is alnum
-    if 3 > (l := len(abbr)): abbr = f'{full[:3-l]}{abbr}'  # each abbr lens 3
-    return abbr.upper()   # upper is more readable, lower solves name confilict
-# Cmd to cache theme as /tmp/$name(mp3)
-mp3 = lambda name, theme: ('sleep 1;curl https://a.animethemes.moe/'  # sleep to obey a rate limit of 60 per minute
-        f"{sorted([v['audio']for e in theme['animethemeentries']for v in e['videos']],key=lambda a:a['size'])[0]['filename']}"
-        f'.ogg|ffmpeg -i - -af loudnorm -b:a 64k -map_chapters -1 -map_metadata -1 -f mp3 /tmp/{name}')
-# > ls 631MUSO[.a-z]*
-# 631MUSO.mp3
-# 631MUSOa.mp3
-#        ^ The dot(.) and lowercase means name confilict, but digit or upper is part of name
-same = lambda name: os.popen(f'ls {name}[.a-z]*').read().split()
-p = argparse.ArgumentParser()
-p.add_argument('since', default='631', nargs='?')  # what season to begin
-a = p.parse_args()
-season, year = int(a.since[2]) - 1, (y := int(a.since[:2])) + (1899, 1999)[63 > y]  # season 0: winter, 1: spring, 2: summer, 3: fall
-newest_date = os.popen('date').read()  # see: if themes updated remote
-while moe := json.load(os.popen("sleep 1;curl -g 'https://api.animethemes.moe/"  # sleep to obey rate limit
-        f'animeyear/{str(year := 1 + year)}'
-        '?fields[anime]=id'  # useless
-        '&fields[animetheme]=sequence,type'  # like'1,OP'
-        '&fields[animethemeentry]=id'  # useless
-        '&fields[audio]=filename,size'
-        '&fields[studio]=slug'  # like'studio_ghibli'
-        '&fields[video]=id'  # useless
-        '&include=animethemes.animethemeentries.videos.audio,studios'
-        "'")):  # Use this to get anison on air in some year
-    for season in range(season, 4):
-        girl = {}
-        for a in moe.get(('winter', 'spring', 'summer', 'fall')[season], ()):
-            s = ''.join(abbr(s['slug']) for s in a['studios'])
-            for t in a['animethemes']:
-                if sames := same(name := f"{str(year)[-2:]}{1+season}{s}{t['type'][0]}{t['sequence']or''}"):
-                    girl.setdefault(name, []).append(t)  # do later
-                else: os.system(f'{mp3(name, t)}&&mv /tmp/{name} {name}.mp3')  # save if complete
-        for name, themes in girl.items():
-            if len(themes) > int(os.popen(f"find ! -newerat '{newest_date}' -name {name}[.a-z]*|wc -l").read()):  # if themes updated remote
-                for theme in themes:  # tell from mds, like'631MUSEa.mp3,631MUSEb.mp3'
-                    os.system(mp3(name, theme))  # cache /tmp/$name
-                    sames = *same(name), f'/tmp/{name}'  # must re-eval sames each time
-                    mds = [''.join(chr(55+ord(c))if c.isdigit()else c for c in os.popen(f'md5sum {s}').read())for s in sames]
-                    for i in range(2):  # conflict rate: 1/256
-                        if len(sames) == len({m[:1+i] for m in mds}):  # mds='a,b'or'aa,ab'
-                            for j, s in enumerate(sames): os.system(f'mv {s} {name}{mds[j][:1+i]}.mp3')
-                            break
-    season = 0  # next year
+import datetime,json,os,sqlite3,urllib.request
+os.makedirs(os.path.expanduser('~/animethemes-audios'),exist_ok=1)
+os.chdir(os.path.expanduser('~/animethemes-audios'))
+for n in*os.listdir(),'18921028'+12*'0':
+    try:
+        less_date=datetime.datetime.strptime(n,'%Y%m%d%H%M%S%f').strftime('%Y-%m-%dT%H:%M:%S.%f')
+        db=n
+        break
+    except:0
+with urllib.request.urlopen(urllib.request.Request(
+    f'https://api.animethemes.moe/audio?fields[audio]=id,link,path,updated_at&filter[updated_at-gt]={less_date}&page[size]=1&sort=updated_at'
+    ,headers={'User-Agent':'curl'}))as r:audio,=json.load(r)['audios']
+with urllib.request.urlopen(audio['link'])as r:moe=r.read()
+with sqlite3.connect(db)as c:
+    c.execute('CREATE TABLE IF NOT EXISTS audios(id UNIQUE,path)')
+    p,=next(c.execute('SELECT path FROM audios WHERE id=:id',audio),[0])
+    if p:
+        os.unlink(p)
+        while 1:
+            try:os.rmdir(p:=os.path.dirname(p))
+            except:break
+        c.execute('DELETE FROM audios WHERE id=:id',audio)
+os.makedirs(os.path.dirname(audio['path']),exist_ok=1)
+with open(audio['path'],'bw')as f:f.write(moe)
+with sqlite3.connect(db)as c:c.execute('INSERT INTO audios VALUES(:id,:path)',audio)
+more_date=datetime.datetime.fromisoformat(audio['updated_at'])
+os.replace(db,more_date.strftime('%Y%m%d%H%M%S%f'))
+if(datetime.datetime.now(datetime.UTC)-more_date).days>7:
+    if'dai'in __file__:os.rename(__file__,__file__.replace('dai','hour'))
+else:
+    if'hour'in __file__:os.rename(__file__,__file__.replace('hour','dai'))
